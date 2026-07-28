e9183232e5b214bf0714f242cbf61068a73c48fb
Dydiddl <dydiddl@naver.com>
2026-07-29 02:54:46 +0900

    feat: add basic NeoVim options

---
+++ lua/config/options.lua
@@ -0,0 +1,40 @@
+local opt = vim.opt
+
+-- line
+opt.number = true
+opt.relativenumber = true
+
+-- cursor spacing
+opt.scrolloff = 5
+opt.sidescrolloff = 5
+
+-- indentation
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.softtabstop = 4
+opt.expandtab = true
+opt.smartindent = true
+
+-- search
+opt.ignorecase = true
+opt.smartcase = true
+opt.incsearch = true
+opt.hlsearch = true
+
+-- screen
+opt.termguicolors = true
+opt.cursorline = true
+opt.signcolumn = "yes"
+opt.wrap = false
+
+-- file handling
+opt.swapfile = false
+opt.backup = false
+opt.undofile = true
+
+-- split window position
+opt.splitright = true
+opt.splitbelow = true
+
+-- system clipboard
+opt.clipboard = "unnamedplus"
