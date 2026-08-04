cc72844e0df4b69da19a16661f304880c27d944f
dydiddl <dydiddl@naver.com>
2026-08-04 21:19:11 +0900

    feat: pick wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -16,31 +16,43 @@ config.color_scheme = "Catppuccin Mocha"
 
 config.enable_tab_bar = true
 config.use_fancy_tab_bar = false
-
+config.initial_cols = 140
+config.initial_rows = 40
 config.window_padding = {
-    left = 8,
-    right = 8,
-    top = 8,
-    bottom = 8,
+	left = 8,
+	right = 8,
+	top = 8,
+	bottom = 8,
 }
+config.window_background_opacity = 0.85
+
+config.window_close_confirmation = "nerverPrompt"
+
+config.freetype_load_target = "Light"
+config.freetype_render_target = "HorizontalLcd"
+
+config.default_cursor_style = "BlinkingBar"
+
+config.enable_scroll_bar = false
+config.hide_tab_bar_if_only_one_tab = true
 
 -- ==================================================
 -- Windows
 -- ==================================================
 
 if is_windows then
-    config.default_domain = "local"
-    config.default_prog = { "pwsh.exe" }
+	config.default_domain = "local"
+	config.default_prog = { "pwsh.exe" }
 end
+
+config.win32_system_backdrop = "Mica"
 
 -- ==================================================
 -- macOS
 -- ==================================================
 
 if is_macos then
-    config.native_macos_fullscreen_mode = true
+	config.native_macos_fullscreen_mode = true
 end
-
-config.hide_tab_bar_if_only_one_tab = true
 
 return config
