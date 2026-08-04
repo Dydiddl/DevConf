94012fa4941f68ec7859885688f59ad922acab78
dydiddl <dydiddl@naver.com>
2026-08-04 14:53:39 +0900

    feat: add conform.nvim with basic formatter configuration

---
+++ nvim/lua/plugins/conform-nvim.lua
@@ -0,0 +1,22 @@
+return {
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				java = { "google-java-format" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				sql = { "sql-formatter" },
+				toml = { "taplo" },
+			},
+
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+		},
+	},
+}
