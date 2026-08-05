b9f47da4843f6a8234125fcaca749e3760b11056
dydiddl <dydiddl@naver.com>
2026-08-05 22:44:38 +0900

    feat: Wezterm setting ubuntu starting point and theme and cursor

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -24,28 +24,47 @@ config.font_size = 13.0
 -- theme
 -- Gruvbox dark, hard (base16)
 -- Hipster Gree
-config.color_scheme = "Hipster Gree"
+-- Homebrew
+-- Cobalt Neon
+-- ENCOM
+-- Harper
+-- JetBrains Darcula
+-- obsidian
+-- cyberpunk
+config.color_scheme = "cyberpunk"
 
+-- window
 config.enable_tab_bar = true
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
+config.enable_scroll_bar = false
 
+-- terminal size
 config.initial_cols = 140
 config.initial_rows = 40
-
 config.window_padding = {
-    left = 8,
-    right = 8,
-    top = 8,
-    bottom = 8,
+    left = 10,
+    right = 10,
+    top = 10,
+    bottom = 10,
 }
 
-config.window_background_opacity = 0
+
+config.window_background_opacity = 1
 config.window_close_confirmation = "NeverPrompt"
 
+-- cursor
+config.animation_fps = 120
+config.default_cursor_style = "BlinkingBlock"
+config.cursor_blink_rate = 1300
+config.cursor_blink_ease_in = "EaseOut"
+config.cursor_blink_ease_out = "EaseOut"
+config.visual_bell = {
+    fade_in_duration_ms = 75,
+    fade_out_duration_ms = 75,
+    target = "CursorColor",
+}
 
-config.default_cursor_style = "SteadyBlock"
-config.enable_scroll_bar = false
 
 -- Keep these only if you prefer their rendering.
 -- config.freetype_load_target = "Light"
@@ -57,7 +76,13 @@ config.enable_scroll_bar = false
 -- ==================================================
 
 if is_windows then
-    config.default_prog = { "pwsh.exe" }
+    config.default_prog = {
+        "wsl.exe",
+        "-d",
+        "Ubuntu-24.04",
+        "--cd",
+        "/home/dydid",
+    }
     config.win32_system_backdrop = "Mica"
 end
 
