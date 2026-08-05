6efaa47091fd90925f0e55f921805e676f84e3f5
dydiddl <dydiddl@naver.com>
2026-08-05 23:10:23 +0900

    feat: Wezterm Setting theme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -14,24 +14,21 @@ local is_macos = wezterm.target_triple:find("darwin") ~= nil
 -- 4 순위 : Hack Nerd Font
 -- 5 순위 : UbuntuMono Nerd Font Mono
 config.font = wezterm.font_with_fallback({
-	"MartianMono Nerd Font Mono",
-	"D2CodingLigature Nerd Font Mono",
-	"Noto Color Emoji",
+    "MartianMono Nerd Font Mono",
+    "D2CodingLigature Nerd Font Mono",
+    "Noto Color Emoji",
 })
 
 config.font_size = 13.0
 
 -- theme
+-- Harper
+-- obsidian
+-- JetBrains Darcula
 -- Gruvbox dark, hard (base16)
 -- Hipster Gree
--- Homebrew
--- Cobalt Neon
--- ENCOM
--- Harper
--- JetBrains Darcula
--- obsidian
 -- cyberpunk
-config.color_scheme = "obsidian"
+config.color_scheme = "cyberpunk"
 
 -- window
 config.enable_tab_bar = true
@@ -43,13 +40,13 @@ config.enable_scroll_bar = false
 config.initial_cols = 140
 config.initial_rows = 40
 config.window_padding = {
-	left = 10,
-	right = 10,
-	top = 10,
-	bottom = 10,
+    left = 10,
+    right = 10,
+    top = 10,
+    bottom = 10,
 }
 
-config.window_background_opacity = 1
+config.window_background_opacity = 0.85
 config.window_close_confirmation = "NeverPrompt"
 
 -- cursor
@@ -59,9 +56,9 @@ config.cursor_blink_rate = 1300
 config.cursor_blink_ease_in = "EaseOut"
 config.cursor_blink_ease_out = "EaseOut"
 config.visual_bell = {
-	fade_in_duration_ms = 75,
-	fade_out_duration_ms = 75,
-	target = "CursorColor",
+    fade_in_duration_ms = 75,
+    fade_out_duration_ms = 75,
+    target = "CursorColor",
 }
 
 -- Keep these only if you prefer their rendering.
@@ -73,14 +70,15 @@ config.visual_bell = {
 -- ==================================================
 
 if is_windows then
-	config.default_prog = {
-		"wsl.exe",
-		"-d",
-		"Ubuntu-24.04",
-		"--cd",
-		"/home/dydid",
-	}
-	config.win32_system_backdrop = "Mica"
+    config.default_prog = {
+        "wsl.exe",
+        "-d",
+        "Ubuntu-24.04",
+        "--cd",
+        "/home/dydid",
+    }
+    -- 투명도를 조절하는데 뒤가 잘 안보이게 하는 옵션
+    -- config.win32_system_backdrop = "Mica"
 end
 
 -- ==================================================
@@ -88,7 +86,7 @@ end
 -- ==================================================
 
 if is_macos then
-	config.native_macos_fullscreen_mode = true
+    config.native_macos_fullscreen_mode = true
 end
 
 return config
