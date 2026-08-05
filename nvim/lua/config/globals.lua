1737abe863dd24da628d6b3c3e21bc9e34790893
dydiddl <dydiddl@naver.com>
2026-08-06 01:07:40 +0900

    feat: NeoVim global config add -> background dark

--- nvim/lua/config/globals.lua
+++ nvim/lua/config/globals.lua
@@ -1,14 +1,15 @@
-vim.g.mapleader = " "      -- global leader
+vim.g.mapleader = " " -- global leader
 vim.g.maplocalleader = " " -- local leader
 
 -- Netrw (Hide the netrw information banner.)
 vim.g.netrw_banner = 0
 
 vim.o.cmdheight = 0
+vim.o.background = "dark"
 
 vim.opt.guicursor = {
-    "n-v-c:block",
-    "i-ci-ve:ver25",
-    "r-cr:hor20",
-    "o:hor90",
+	"n-v-c:block",
+	"i-ci-ve:ver25",
+	"r-cr:hor20",
+	"o:hor90",
 }
