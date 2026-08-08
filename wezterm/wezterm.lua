0ffa7e83f99eaf9299ef647db38f372ff6a45088
dydiddl <dydiddl@naver.com>
2026-08-08 21:01:17 +0900

    feat(wezterm): change theme -> obsidian

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -11,8 +11,8 @@ local is_macos = wezterm.target_triple:find("darwin") ~= nil
 -- ==================================================
 
 wezterm.on("gui-startup", function(cmd)
-    local _, _, window = mux.spawn_window(cmd or {})
-    window:gui_window():set_position(29, 50)
+	local _, _, window = mux.spawn_window(cmd or {})
+	window:gui_window():set_position(29, 50)
 end)
 
 -- ==================================================
@@ -26,9 +26,9 @@ end)
 -- 5 순위 : UbuntuMono Nerd Font Mono
 
 config.font = wezterm.font_with_fallback({
-    "MartianMono Nerd Font Mono",
-    "D2CodingLigature Nerd Font Mono",
-    "Noto Color Emoji",
+	"MartianMono Nerd Font Mono",
+	"D2CodingLigature Nerd Font Mono",
+	"Noto Color Emoji",
 })
 config.font_size = 12.5
 
@@ -39,7 +39,7 @@ config.font_size = 12.5
 -- Gruvbox dark, hard (base16)
 -- Hipster Gree
 -- cyberpunk
-config.color_scheme = "Harper"
+config.color_scheme = "obsidian"
 
 -- window
 config.enable_tab_bar = true
@@ -51,10 +51,10 @@ config.enable_scroll_bar = false
 config.initial_cols = 191
 config.initial_rows = 60
 config.window_padding = {
-    left = 10,
-    right = 10,
-    top = 10,
-    bottom = 10,
+	left = 10,
+	right = 10,
+	top = 10,
+	bottom = 10,
 }
 
 config.window_background_opacity = 0.75
@@ -62,15 +62,15 @@ config.window_close_confirmation = "NeverPrompt"
 
 -- cursor
 
-config.animation_fps = 120
+-- config.animation_fps = 120
 config.default_cursor_style = "BlinkingBlock"
 config.cursor_blink_rate = 1300
 config.cursor_blink_ease_in = "EaseOut"
 config.cursor_blink_ease_out = "EaseOut"
 config.visual_bell = {
-    fade_in_duration_ms = 75,
-    fade_out_duration_ms = 75,
-    target = "CursorColor",
+	fade_in_duration_ms = 75,
+	fade_out_duration_ms = 75,
+	target = "CursorColor",
 }
 
 -- Keep these only if you prefer their rendering.
@@ -82,15 +82,15 @@ config.visual_bell = {
 -- ==================================================
 
 if is_windows then
-    config.default_prog = {
-        "wsl.exe",
-        "-d",
-        "Ubuntu-24.04",
-        "--cd",
-        "/home/dydid",
-    }
-    -- 투명도를 조절하는데 뒤가 잘 안보이게 하는 옵션
-    -- config.win32_system_backdrop = "Mica"
+	config.default_prog = {
+		"wsl.exe",
+		"-d",
+		"Ubuntu-24.04",
+		"--cd",
+		"/home/dydid",
+	}
+	-- 투명도를 조절하는데 뒤가 잘 안보이게 하는 옵션
+	-- config.win32_system_backdrop = "Mica"
 end
 
 -- ==================================================
@@ -98,7 +98,7 @@ end
 -- ==================================================
 
 if is_macos then
-    config.native_macos_fullscreen_mode = true
+	config.native_macos_fullscreen_mode = true
 end
 
 return config
